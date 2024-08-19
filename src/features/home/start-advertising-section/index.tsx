d406291f52961883f3e43f235744ef5f775bc488
wahibadiyatma17 <wahib.adiyatma17@gmail.com>
2024-08-19 21:48:58 +0700

    feat: added firebase integration

--- src/features/home/start-advertising-section/index.tsx
+++ src/features/home/start-advertising-section/index.tsx
@@ -2,8 +2,42 @@ import { Input, Select } from 'antd';
 import { FC } from 'react';
 import toast from 'react-hot-toast';
 import styled from 'styled-components';
+import { collection, addDoc } from 'firebase/firestore';
+import db from '@/lib/firestore';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type FormInput = {
+  fullname: string;
+  email: string;
+  businessEntityName: string;
+  budget: string;
+};
 
 const StartAdvertisingSection: FC = () => {
+  const { handleSubmit, setValue, reset } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    if (!data.fullname && !data.email && !data.budget && !data.businessEntityName)
+      return toast.error('Form can not be empty');
+    if (!data.fullname) return toast.error('Fullname can not be empty');
+    if (!data.email) return toast.error('Email can not be empty');
+    if (!data.businessEntityName) return toast.error('Business/entity name can not be empty');
+    if (!data.budget) return toast.error('Advertising Budget can not be empty');
+
+    try {
+      const docRef = await addDoc(collection(db, 'advertise-form'), {
+        fullname: data.fullname,
+        email: data.email,
+        businessEntityName: data.businessEntityName,
+        budget: data.budget,
+      });
+      toast.success('Message sent!');
+      reset();
+    } catch (e) {
+      console.log('wahib e', e);
+      toast.error('Server Error');
+    }
+  };
+
   return (
     <section className="bg-[#EDEFF7]" id="start-advertise-now">
       <div className="w-full py-12 md:py-24 lg:py-32 bg-[#EDEFF7] flex items-center justify-center">
@@ -19,15 +53,28 @@ const StartAdvertisingSection: FC = () => {
             </div>
             <div className="mx-auto w-full max-w-sm sm:max-w-md md:max-w-lg space-y-4">
               <form className="grid gap-4">
-                <Input placeholder="Fullname" size={'large'} />
-                <Input placeholder="Email" size={'large'} />
-                <Input placeholder="Business/Entity Name" size={'large'} />
+                <Input
+                  placeholder="Fullname"
+                  size={'large'}
+                  onChange={(e) => setValue('fullname', e.target.value)}
+                />
+                <Input
+                  placeholder="Email"
+                  size={'large'}
+                  onChange={(e) => setValue('email', e.target.value)}
+                />
+                <Input
+                  placeholder="Business/Entity Name"
+                  size={'large'}
+                  onChange={(e) => setValue('businessEntityName', e.target.value)}
+                />
 
                 <StyledSelect
                   size={'large'}
                   style={{ width: '100%' }}
                   placeholder={'Advertising Budget (USD)'}
                   allowClear
+                  onChange={(e) => setValue('budget', e as string)}
                   options={[
                     { value: '<100', label: '<100' },
                     { value: '100-500', label: '100-500' },
@@ -40,7 +87,7 @@ const StartAdvertisingSection: FC = () => {
 
                 <div
                   className="w-full bg-[#022739] text-white py-2 rounded-lg cursor-pointer text-center"
-                  onClick={() => toast.success('Message sent!')}
+                  onClick={handleSubmit(onSubmit)}
                 >
                   Send
                 </div>
---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRf0Z1PVEEBu0hGGPvzFVMYlnooY4DDU4',
+  authDomain: 'marketing-a7e57.firebaseapp.com',
+  projectId: 'marketing-a7e57',
+  storageBucket: 'marketing-a7e57.appspot.com',
+  messagingSenderId: '130899643717',
+  appId: '1:130899643717:web:1ba2e7c07648746a36c22c',
+  measurementId: 'G-BSVDSKY92E',
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
