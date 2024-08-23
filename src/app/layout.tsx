551f14d8fbb8dce024aab334518ce256e68cf209
wahibadiyatma17 <wahib.adiyatma17@gmail.com>
2024-08-23 07:33:08 +0700

    feat: update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/logo-lq.png" type="image/png" sizes="32x32" />
         <link
           rel="stylesheet"
